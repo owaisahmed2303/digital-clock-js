a8cf1beb84b25f70a3599d244c9d5a4014367603
owaisahmed2303 <owaisahmed2303@gmail.com>
2026-09-19 11:44:57 +0500

    UI

--- app.js
+++ app.js
@@ -2,35 +2,39 @@ var h = document.getElementById('span-hours');
 var m = document.getElementById('span-minutes');
 var s = document.getElementById('span-seconds');
 var am_pm = document.getElementById('am-pm');
+var dayslist=document.querySelectorAll('#days-list li');
+
+// console.log(dayslist)
 
 
 function digitalclock() {
     var time = new Date();
-
     var hours = time.getHours();
     var minutes = time.getMinutes();
     var seconds = time.getSeconds();
-    
+    var weekdaysIndex=time.getDay();
+   
+     weekdays=['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
 
+    
+    if (time.getHours() >= 12) {
+        am_pm.innerText = 'PM';
+    } else {
+        am_pm.innerText = 'AM';
+    }
+    hours = hours % 12;
+    if (hours === 0) {
+        hours = 12;
+    }
 
     h.innerText = hours;
     m.innerText = minutes;
     s.innerText = seconds;
-    
 
+    dayslist[weekdaysIndex].style.color='#003bea';
+    dayslist[weekdaysIndex].style.fontWeight='bold';
     
-    if (hours >= 12 ) {
-        am_pm.innerText = 'pm';
-        h.innerText = hours - 12;
-    }
-    else{
-        am_pm.innerText = 'am';
-        if(hours==0){
-      
-        hours=12;
-    }
-    }
-
+    
 
 }
 
